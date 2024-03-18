c730f70cb48c53e5da6ce74688e1d539f8aef702
NiveditaSpeakAi <nivedita@speakai.co>
2024-03-18 22:39:22 +0530

    new app changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Schitt's Creek</h1>
+      <br />
+      <br />
+      <hr />
+      <div class="container">
+        <img
+          src="https://static0.srcdn.com/wordpress/wp-content/uploads/2020/01/schitts-creek-cast-photo-Edited.jpg"
+          alt="Schitt's Creek"
+          style={{ width: "100%" }}
+        />
+      </div>
+      <br />
+      <br />
+      <hr></hr>
+      <p>
+        Schitt's Creek is a Canadian sitcom that follows the Rose family, who
+        lose their fortune and move to a small town they once owned as a joke.{" "}
+      </p>
+      <br />
+      <h2>Couples list with their partners</h2>
+      <table>
+        <tbody>
+          <tr>
+            <th>Partner 1</th>
+            <th>Partner 2</th>
+          </tr>
+          <tr>
+            <td>Ted Mullens</td>
+            <td>Alexis Rose</td>
+          </tr>
+          <tr>
+            <td>David Rose</td>
+            <td>Patrick Brewer</td>
+          </tr>
+          <tr>
+            <td>Emir Kaplan</td>
+            <td>Stevie Budd</td>
+          </tr>
+          <tr>
+            <td>Jhonny Rose</td>
+            <td>Moira Rose</td>
+          </tr>
+          <tr>
+            <td>Roland Schitt</td>
+            <td>Jocelyn Schitt</td>
+          </tr>
+          <tr>
+            <td>Mutt Schitt</td>
+            <td>Twyla Sands</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
