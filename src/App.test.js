25a244bae8f16063da6b4bd34c26076576088c19
NiveditaSpeakAi <nivedita@speakai.co>
2024-03-19 00:01:08 +0530

    update

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+describe("Register component", () => {
+  it("should render Register component correctly", () => {
+    render(<App />);
+    const element = screen.getByText("Schitt's Creek");
+    expect(element).toBeInTheDocument();
+  });
 });
